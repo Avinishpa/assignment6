16aa2c2976d45ead1c3041578238ccc627a0fe23
Avinishpa <122361520+Avinishpa@users.noreply.github.com>
2023-05-15 20:47:10 +0530

    Add files via upload

---
+++ myapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Book_Model(models.Model):
+    Title = models.CharField(max_length=150)
+    Author = models.CharField(max_length=150)
+    Isbn = models.CharField(max_length=100)
+    Publisher = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Title
+        
